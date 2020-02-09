e430490810d782b3ad127e88bf392b0c21f73fa0
SubZiro <webnlevel@gmail.com>
2020-02-09 21:37:54 +0400

    Create main.py

---
+++ app_with_doctor/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
+
+import config
+
+
+def auth(driver, user, id_user):
+	# --ожидание появления всплывающего окна-- #
+	driver.implicitly_wait(6) # ждём 6 секунд, по умолчанию - 0
+	driver.window_handles 
+	# --ввод данных-- #
+	name = driver.find_element_by_id('login-f')
+	name.send_keys(user)
+	user_id = driver.find_element_by_id('login-p')
+	user_id.send_keys(id_user)
+	# --вход-- #
+	btn = driver.find_element_by_id('login-Проверить')
+	btn.click()
+	# --ожидание авторизации пользователя-- #
+	wait = WebDriverWait(driver, 15).until(
+		EC.visibility_of_element_located((By.ID,'divlogin')))
+	# --завершение авторизации-- #
+	connected = driver.find_element_by_css_selector('a').click()
+	# --обновление страницы-- #
+	sleep(1.3)
+	driver.refresh()
+
+
+def get_btn_to_record(driver):
+	'''--получение кликабельных конпок для записи--'''
+	#btn_grey = driver.find_elements(By.TAG_NAME, 'Не активный номерок (откроется в 20.00)')
+	#btn_orange = driver.find_elements(By.TAG_NAME, 'Номерок занят')
+	btn_all = driver.find_elements_by_css_selector('a.btn.green')
+	btn_grey = [elem for elem in btn_all if elem.get_attribute('title') != 'Номерок занят']
+	return btn_grey
+
+
+def get_not_empty_btn(driver, btn_grey):
+	'''--кликание по всем полученым кнопкам записи--'''
+	flg = False
+	for elem in btn_grey:
+		try:
+			elem.click()
+			alert = driver.switch_to_alert().accept()
+		except Exception as e:
+			flg = True
+			break
+	return flg
+
+
+def record_to_doc(driver):
+	'''запись к специалисту'''
+	try:
+		btn_wait = WebDriverWait(driver, 15).until(
+			EC.visibility_of_element_located((By.ID,'zapis-Записаться'))).click()
+		# ---- #
+		WebDriverWait(driver, 15).until(EC.alert_is_present())
+		alert = driver.switch_to_alert().accept()
+		return True
+	except Exception as e:
+		print(e.message)
+		return False
+
+
+def main():
+	driver = webdriver.Chrome(config.chromedriver)
+	driver.get(config.url)
+	#--авторизация--#
+	driver.find_element_by_css_selector('a.btn.blue').click()
+	auth(driver, config.s_name, config.id_user)
+	# --получение кнопок для записи к специалисту-- #
+	all_btn = get_btn_to_record(driver)
+	# --если есть свободные места для записи, записываем в первую свободную-- #
+	if get_not_empty_btn(driver, all_btn):
+		flg_rec = record_to_doc(driver)
+		print('Вроде записал, проверяй' if flg_rec else 'Не удалось, давай в следующий раз')
+	else:
+		print('Нет свободных номерков')
+	# --закрытие браузера-- #
+	sleep(5)
+	driver.quit()
+
+
+if __name__ == '__main__':
+	main()
