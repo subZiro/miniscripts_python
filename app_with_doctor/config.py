3b1e45e1f8622566d526981d74c1b42415e9525f
SubZiro <webnlevel@gmail.com>
2020-02-09 21:49:46 +0400

    Create config.py

---
+++ app_with_doctor/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+	print('(-_-)')
+else:
+	chromedriver = '/полный/путь/к/файлу/mini_scripts/making_an_appointment/chromedriver'
+	s_name = 'фамилия'
+	id_user = 'номер пользователя'
+	url = 'url специалиста'
+
